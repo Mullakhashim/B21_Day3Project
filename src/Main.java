1fcc2b1729854dfa363b0df4d101c921687e8056
Mullakhashim <mullahashimsimai@gmail.com>
2020-11-13 21:55:49 -0500

    working on the project "2"

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git Last Day");
+        //1
+        //2
+        //3
 
     }
 }
